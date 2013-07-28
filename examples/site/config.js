57cfeca604a453f47fa60de720765d54cca43d9f
Derek Chiang <derekchiang93@gmail.com>
2013-07-28 02:04:54 -0700

    Bug fix

--- examples/site/config.js
+++ examples/site/config.js
@@ -2,7 +2,7 @@ module.exports = {
   indexPage: 'about',
   baseView: 'index',
   siteParams: {
-    siteTitle: "Derek's Site",
-    siteSubtitle: 'Winner of this year\'s "Most Creative Site Title" award',
+    title: "Derek's Site",
+    subtitle: 'Winner of this year\'s "Most Creative Site Title" award',
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,18 @@ var sitePath = path.join(__dirname, '..' + path.sep, config.siteDir)
 
 var siteConfig = require(path.join(sitePath, 'config'))
 
+function simpleClone(obj) {
+  return JSON.parse(JSON.stringify(obj))
+}
+
 function commonRender(res, url) {
   var pagePath = path.join(config.siteDir, "pages", url) + '.md'
 
   if (fs.existsSync(pagePath)) {
     fs.readFile(pagePath, 'utf8', function(err, data) {
       if (err) throw err
-      var siteParams = siteConfig.siteParams || {}
+      var siteParams = simpleClone(siteConfig.siteParams) || {}
+      console.log(siteParams)
       siteParams.content = marked(data)
       res.render(siteConfig.baseView, siteParams)
     })
